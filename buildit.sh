4c9683cc8058df3d1b21c771005683545010211e
nutz <legerde@gmail.com>
2016-08-13 15:05:08 -0600

    Add Build Script. CryptoNoteConfig.h is better.

---
+++ buildit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+STR="0-0-0-1"
+
+rm -Rf ~/.bipcoin
+
+#make it
+#cmake .
+#make
+
+#archive it
+rm -Rf build
+mkdir build
+mkdir build/bipcoin
+cp src/bipcoind build/bipcoin
+cp src/connectivity_tool build/bipcoin
+cp src/simplewallet build/bipcoin
+cp src/walletd build/bipcoin
+cd build
+tar -cvf bipcoin.linux.x64.v$STR.tar  bipcoin/* 
+gzip -9 bipcoin.linux.x64.v$STR.tar
+
